beceb45346575f164f558af3ed33cac56b105963
paulbrowne <Paulmatthewbrowne@gmail.com>
2017-11-03 20:40:20 +0200

    adds debounced resize on width only

--- fit-text-on-lines.js
+++ fit-text-on-lines.js
@@ -1,16 +1,30 @@
 /*! fit-text-on-lines.js | Paul Browne */
-(function() {
-  
-    function debouncedResize(a, b) {
+function fittextonlines() {
+    function bodyHasScrollbar(){
+        if (document.documentElement.getBoundingClientRect().height > window.innerHeight){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    function debouncedResize(b, c) {
+        var d = [window.innerWidth];
         return window.addEventListener("resize", function() {
-            clearTimeout(b),
-                b = setTimeout(a, 150)
-        }), a
+            var e = window.innerWidth,
+                f = d.length;
+            d.push(e);
+            if (d[f] !== d[f - 1]) {
+                clearTimeout(c);
+                c = setTimeout(b, 100);
+            }
+        }), b;
     }
     debouncedResize(function() {
         var element = document.querySelectorAll(".js-fit-text-on-lines, .js-ftol");
-        var i = element.length;
-        while (i--) {
+        var j = element.length;
+        var i = 0;
+        var noScrollbar = 0;
+        while (i<j) {
             element[i].setAttribute("style", "");
             element[i].style.fontSize = "256px";
             var lines = parseFloat(getComputedStyle(element[i]).minHeight) || 1;
@@ -20,33 +34,31 @@
             var lh = lineHeight / 256;
             var inc = 128;
             var beforeFontSize;
-                      
             decreaseFontSize();
-          
             function increaseFontSize() {
-                if (inc >= 0.5) {
-                    while (linesUsed <= lines && inc >= 0.5) {
+                if (inc >= 0.25) {
+                    while (linesUsed <= lines && inc >= 0.25) {
                         beforeFontSize = element[i].style.fontSize;
                         element[i].style.fontSize = parseFloat(element[i].style.fontSize) + inc + "px";
                         linesUsed = Math.round(element[i].scrollHeight / (lh * parseFloat(element[i].style.fontSize)));
-                        if (beforeFontSize == element[i].style.fontSize && inc < 1) {                       
+                        if (beforeFontSize == element[i].style.fontSize && inc < 1) {
                             inc = 0.1;
                             lines = 0;
                         }
-                    inc = inc / 2;                      
+                    inc = inc / 2;
                     }
                     decreaseFontSize();
                 }
             }
             function decreaseFontSize() {
-                if (inc >= 0.25) {
-                    while (linesUsed > lines && inc >= 0.25) {              
+                if (inc >= 0.125) {
+                    while (linesUsed > lines && inc >= 0.125) {
                         beforeFontSize = element[i].style.fontSize;
                         element[i].style.fontSize = parseFloat(element[i].style.fontSize) - inc + "px";
-                        linesUsed = Math.round(element[i].scrollHeight / (lh * parseFloat(element[i].style.fontSize)));                      
-                        if (inc == 0.25) {
+                        linesUsed = Math.round(element[i].scrollHeight / (lh * parseFloat(element[i].style.fontSize)));
+                        if (inc == 0.125) {
                             element[i].style.fontSize = parseFloat(element[i].style.fontSize) - inc + "px";
-                        }                      
+                        }
                         if (beforeFontSize == element[i].style.fontSize && inc < 1) {
                             inc = 0.1;
                             linesUsed = 0;
@@ -56,6 +68,18 @@
                     increaseFontSize();
                 }
             }
+            if (bodyHasScrollbar()){
+                noScrollbar++;
+            }
+            i++;
+            if (document.body.style.overflowY != "scroll" && noScrollbar && i == j){
+                document.body.style.overflowY = "scroll";
+                noScrollbar = 0;
+                i = 0;
+            }
+        }
+        if(document.body){
+            document.body.style.overflowY = "";
         }
     })();
-})();
+}
