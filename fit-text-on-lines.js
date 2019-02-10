c86987eacee4a54442900a9eac23ee29126db945
paulbrowne <Paulmatthewbrowne@gmail.com>
2019-02-10 16:07:44 +0200

    removes return from function

--- fit-text-on-lines.js
+++ fit-text-on-lines.js
@@ -4,20 +4,19 @@
 		z.ftol = function(){
 			var ftols = document.querySelectorAll(".ftol");
 			var i = ftols.length;
-			function reCalc(obj, el, inc){
+			function calculate(obj, el, inc){
 				el.style.fontSize = parseFloat(getComputedStyle(el).fontSize) + inc + "px";
 				obj.height = el.getBoundingClientRect().height;
 				obj.lineHeight = Math.floor(parseFloat(getComputedStyle(el).lineHeight)) * (parseFloat(getComputedStyle(ftols[i]).minHeight) || 1);
-				return obj;
 			}
 			while(i--){
-				var dims = {};
-				reCalc(dims, ftols[i], 0);
-				while(dims.height <= dims.lineHeight){
-					reCalc(dims, ftols[i], 1);
+				var dimensions = {};
+				calculate(dimensions, ftols[i], 0);
+				while(dimensions.height <= dimensions.lineHeight){
+					calculate(dimensions, ftols[i], 1);
 				}
-				while(dims.height > dims.lineHeight){
-					reCalc(dims, ftols[i], -1);
+				while(dimensions.height > dimensions.lineHeight){
+					calculate(dimensions, ftols[i], -1);
 				}
 			}
 		}
